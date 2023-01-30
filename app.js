5284305bf1bace5f99c789ec070e1de664a00910
Nitya-108 <clinton.moodley@protonmail.com>
2023-01-30 21:38:43 +0530

    Min hand corrected
    correct clock and complete

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ let mm = day.getMinutes() * deg;
 let ss = day.getSeconds() * deg; 
 
 hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
-mn.style.transform = `rotateZ(${mn}deg)`;
+mn.style.transform = `rotateZ(${mm}deg)`;
 sc.style.transform = `rotateZ(${ss}deg)`;
+
 
 });
